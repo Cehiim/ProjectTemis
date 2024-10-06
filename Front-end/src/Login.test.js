ea6befb21ea3153321d12f325ecd66fc8fa0339c
David Pessoa <104323068+david-pessoa@users.noreply.github.com>
2024-10-05 21:59:10 -0300

    Update and rename App.test.js to Login.test.js

--- Front-end/src/Login.test.js
+++ Front-end/src/Login.test.js
@@ -1,12 +1,12 @@
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Login from './Login';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.createRoot(div).render(
     <StrictMode>
-      <App />
+      <Login />
     </StrictMode>
   );
 });
