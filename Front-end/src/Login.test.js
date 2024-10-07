ea517909c448b6c7dfa52ae3550231b8b2fc8968
david-pessoa <pessoa.dvlbp@gmail.com>
2024-10-06 21:24:46 -0300

    finaliza testes de unidade do front para a esteira CI/CD

--- Front-end/src/Login.test.js
+++ Front-end/src/Login.test.js
@@ -1,6 +1,7 @@
 import React, {StrictMode} from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Login from './Login';
+import {render, screen} from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -9,4 +10,32 @@ it('renders without crashing', () => {
       <Login />
     </StrictMode>
   );
+});
+
+test('verifica se há a fase "email de login:"', () => {
+  render(<Login />)
+  const input = screen.getByText('Email de login:')
+
+});
+
+test('verifica se há a fase "Senha:"', () => {
+  render(<Login />)
+  const input = screen.getByText('Senha:')
+
+});
+
+test('verifica se há botão de login', () => {
+  render(<Login />)
+  const input = screen.getByRole('button')
+
+});
+
+test('verifica se há botão para cadastrar-se', () => {
+  render(<Login />)
+  const input = screen.getByText('Cadastre-se aqui')
+});
+
+test('verifica se há botão para em caso o usuário tenha esquecido a senha', () => {
+  render(<Login />)
+  const input = screen.getByText('Esqueci minha senha')
 });
