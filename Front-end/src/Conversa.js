9b6e77f2973d2b070ab25549b15b4f03d7f755b1
david-pessoa <pessoa.dvlbp@gmail.com>
2024-10-10 20:42:30 -0300

    corige input

--- Front-end/src/Conversa.js
+++ Front-end/src/Conversa.js
@@ -20,15 +20,15 @@ function Conversa(){
         marginLeft: '5vh',
         marginRight: '70vw',
     }
-    /*
+    
     const InputStyle={
         position: 'fixed', // Posiciona o elemento em relação à janela de visualização
         left: '5vw',
-        width: '10vw',
-        bottom: '30px',
+        width: '90vw',
+        bottom: '4vh',
         display: 'flex',
         justifyContent: 'center',
-    } */
+    }
 
         // Define um estado para controlar se o texto está visível ou não
         const [isVisible, setIsVisible] = useState(true);
@@ -40,25 +40,27 @@ function Conversa(){
 
         /*
         <BemVindo isVisible={isVisible}/> //Objetivo é incluir a mensagem de Bem Vindo, mas deixa pra depois
-        <Chat/>
+        
         <div style = {InputStyle}>
             <Input onToggleText={toggleText}/>
         </div>
+        <Chat/>
         */
 
     return(
-        <body>
+    <body>
         <div style = {TopoStyle}>
             <img src={menu_hamburguer}/>
             <h1 style = {TitleStyle}>SamsAI</h1> 
             <img src={profile_photo}/>
         </div>
 
+        <BemVindo isVisible={isVisible}/>
         
-            <Chat/>
-        
-        
-
+        <div style = {InputStyle}>
+            <Input onToggleText={toggleText}/>
+        </div>
+    
     </body>
     );
 }
